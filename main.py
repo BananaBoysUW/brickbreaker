bfd4581303e923b698d1eeff39f3bdd9d16916ba
Muneeb-F <m5farruk@uwaterloo.ca>
2022-11-16 20:42:13 -0500

    this version can draw polygons

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import pygame
 import random
 from pygame import mixer
 
+
 # Initialize the pygame
 pygame.init()
 
@@ -24,12 +25,73 @@ paddleX = 335
 paddleY = 550
 paddleX_change = 0
 
+#Brick Coordinates with Structure
+# brickCoordinates = [[[30,30],[30,50],[50,80],[80,30]],
+#                     [[50,100],[100,40],[40,65]],
+#                     [[100,200],[200,80],[350,100]]]
+
+
+# IN THE FINAL VERSION THIS ARRAY WILL BE GIVEN BY THE IMAGE ALGORITHM
+# brickCoordinates = [[[200,200],[200,400],[250,400],[350,325],[450,400],[500,400],[500,200],[450,200],[450,325],[350,275],[250,325],[250,200]]]
+brickCoordinates = [[[200,100],[200,200],[400,200],[400,100]],[[600,200],[700,200],[650,300]],[[250,250],[400,250],[450,350],[300,350]]]
+
+print(brickCoordinates[0][0][0])
+brickMasks = []
+
+#To see how many times to iterate each loop
+
+def getSizeOfNestedList(listOfElem):
+    #Number of shapes
+    count = 0
+    # Iterate over the list
+    for elem in brickCoordinates:
+        # Check if type of element is list
+        if type(elem) == list:  
+            # Again call this function to get the size of this element
+            count += getSizeOfNestedList(elem)
+        else:
+            count += 1    
+    return count
+
+length = len(brickCoordinates)
+print('Number of shapes = ', length)
+
+
+count = 0
+for listElem in brickCoordinates:
+    count += len(listElem)                    
+print('Total Number of points in each shape : ', count)
+
+count = count/len(brickCoordinates)
+count = count - 1
+points = int(count)
+
+
+brickCoorX = []
+brickCoorY = []
+
+# How many shapes there are
+for i in range(len(brickCoordinates)):
+    for j in range(points + 1):  
+        brickCoorX.append(brickCoordinates[i][j][0])
+        brickCoorY.append(brickCoordinates[i][j][1])
+
+
+print(*brickCoorX, sep = ", ")
+print(*brickCoorY, sep = ", ")
+
+
+
+
+
+
 # BrickCoordinates
-brickX = []
-brickY = []
+# brickX = []
+# brickY = []
+
 brickX_change = []
 brickY_change = []
-numOfBricks = 600
+numOfBricks = len(brickCoordinates)
 numOfRows = 100
 
 #Bricks
@@ -42,8 +104,8 @@ pinkBrickImage = pygame.image.load("assets/images/pinkBrick.png")
 ballImage = pygame.image.load("assets/images/Ball.png")
 ballX = 380
 ballY = 280
-ballX_change = 2.5
-ballY_change = 2.5
+ballX_change = 0.25
+ballY_change = 0.25
 
 #Score
 scoreValue = 0
@@ -72,22 +134,22 @@ def gameOverText():
 def paddle(x, y):
     screen.blit(paddleImage, (x, y))
 
-def yellowBrick(x, y, i):
-    screen.blit(yellowBrickImage, (x, y))
+# def yellowBrick(x, y, i):
+#     screen.blit(yellowBrickImage, (x, y))
 
-def greenBrick(x, y, i):
-    screen.blit(greenBrickImage, (x, y))
+# def greenBrick(x, y, i):
+#     screen.blit(greenBrickImage, (x, y))
 
-def blueBrick(x, y, i):
-    screen.blit(blueBrickImage, (x, y))
+# def blueBrick(x, y, i):
+#     screen.blit(blueBrickImage, (x, y))
 
-def pinkBrick(x, y, i):
-    screen.blit(pinkBrickImage, (x, y))
+# def pinkBrick(x, y, i):
+#     screen.blit(pinkBrickImage, (x, y))
 
 def ball(x, y):
     screen.blit(ballImage, (x, y))
 
-#To pick random brick colours
+# #To pick random brick colours
 colourOfBrick = []
 for i in range(numOfBricks):
     colourOfBrick.append(random.randint(1,4))
@@ -95,19 +157,19 @@ for i in range(numOfBricks):
 brickXValue = 15
 brickYValue = 0
 
-for i in range(numOfRows):
+# for i in range(numOfRows):
 
-    for j in range(numOfBricks):
+#     for j in range(numOfBricks):
 
-        brickX.append(brickXValue)
-        brickY.append(brickYValue)
-        brickXValue += 130
-        brickX_change.append(0.1)
-        brickY_change.append(0.1)
-        if len(brickX)%6 == 0:
-            brickXValue = 15
-        if len(brickY)%6 == 0:
-            brickYValue -= 50  # row height
+#         brickX.append(brickXValue)
+#         brickY.append(brickYValue)
+#         brickXValue += 130
+#         brickX_change.append(0.01)
+#         brickY_change.append(0.01)
+#         if len(brickX)%6 == 0:
+#             brickXValue = 15
+#         if len(brickY)%6 == 0:
+#             brickYValue -= 50  # row height
 
 
 # Game Loop (makes sure game is always running)
@@ -124,14 +186,14 @@ while running:
         # If keystroke is pressed check whether left or right
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_LEFT:
-                paddleX_change = -7
+                paddleX_change = -2
             if event.key == pygame.K_RIGHT:
-                paddleX_change = 7
+                paddleX_change = 2
         if event.type == pygame.KEYUP:
             if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
                 paddleX_change = 0
 
-    # Checking boudries of paddle
+    # Checking boundaries of paddle
     paddleX += paddleX_change
 
     if paddleX <= 0:
@@ -140,34 +202,45 @@ while running:
         paddleX = 669
 
     #Draw Rectangles around bricks
-    brickRect = []
-    for i in range(numOfBricks):
-        brickRect.append(pygame.draw.rect(screen, (0, 0, 0), (brickX[i], brickY[i], 120, 42),1))
+    # brickRect = []
+    brickPolygon = []
+    for i in range(len(brickCoordinates)):
+        # brickPolygon.append(pygame.draw.polygon(screen, (0, 0, 0), ((brickCoorX[i], brickCoorY[i]), (brickCoorX[i+1],brickCoorY[i+1]), (brickCoorX[i+2], brickCoorY[i+2]))))
+        
+        brickPolygon.append(pygame.draw.polygon(screen, (0, 0, 0), brickCoordinates[i]))
+        # brickMasks.append(pygame.mask.Mask(brickCoorX[i],brickCoorY[i]))
+        
+    # #TESTING DRAWING POLYGONS
+    # brickRect = []
+    # for i in range(numOfBricks):
+    #     brickRect.append(pygame.draw.polygon(screen, (0, 0, 0), ([brickCoordinates[0],brickCoordinates[1]]),1))
 
-    for i in range(numOfBricks):
+
+
+    for i in range(len(brickCoordinates)):
         #Game Over
-        if brickY[i] >= 512:
-            for j in range(numOfBricks):
+        if brickCoorY[i] >= 512:
+            for j in range(len(brickCoordinates)):
                 if playGameOverSound == True:
                     gameOverSound.play()
-                brickX[j] = -2000
+                brickCoorX[j] = -2000
                 ballX_change = 0
                 ballY_change = 0
                 playGameOverSound = False
             gameOverText()
-        # Brick Movement
-        brickY[i] += brickY_change[i]
-        if brickY[i] <= 0:
-            brickY_change[i] = 0.1
+        # # Brick Movement
+        # brickCoorY[i] += brickY_change[i]
+        # if brickCoorY[i] <= 0:
+        #     brickY_change[i] = 0.01
         # Makes brick show up on screen
-        if colourOfBrick[i] == 1:
-            yellowBrick(brickX[i], brickY[i], i)
-        elif colourOfBrick[i] == 2:
-            greenBrick(brickX[i], brickY[i], i)
-        elif colourOfBrick[i] == 3:
-            blueBrick(brickX[i], brickY[i], i)
-        elif colourOfBrick[i] == 4:
-            pinkBrick(brickX[i], brickY[i], i)
+        # if colourOfBrick[i] == 1:
+        #     yellowBrick(brickCoorX[i], brickCoorY[i], i)
+        # elif colourOfBrick[i] == 2:
+        #     greenBrick(brickCoorX[i], brickCoorY[i], i)
+        # elif colourOfBrick[i] == 3:
+        #     blueBrick(brickCoorX[i], brickCoorY[i], i)
+        # elif colourOfBrick[i] == 4:
+        #     pinkBrick(brickCoorX[i], brickCoorY[i], i)
 
 
     # Ball Movement and boundary checking
@@ -189,25 +262,40 @@ while running:
 
 
     # Paddle and Ball Collision
-    if ballY > 530 and ballY < 535 and (ballX+20) < paddleX + 131 and (ballX+20) > paddleX:
+    if ballY > 530 and ballY < 535 and (ballX+20) < paddleX + 138 and (ballX+20) > paddleX:
         paddleSound = mixer.Sound("assets/sounds/hitPaddleSoundEffect.wav")
         paddleSound.play()
         ballY_change *= -1
 
     paddle(paddleX, paddleY)
-    ballCircle = pygame.draw.circle(screen, (0,0,0), (int(ballX+20),int(ballY+20)) ,20)
     ball(ballX, ballY)
+    
+    
+    
+    ballCircle = pygame.draw.circle(screen, (0,0,0), (int(ballX+20),int(ballY+20)) ,20)
+    ballMask = pygame.mask.from_surface(screen)
 
-    #Ball and Brick Collision
-    for i in range (numOfBricks):
-        if ballCircle.colliderect(brickRect[i]):
-            if abs(ballCircle.top - brickRect[i].bottom < 10) and ballY_change < 0:
-                brickSound = mixer.Sound("assets/sounds/hitBrickSoundEffect.wav")
-                brickSound.play()
-                brickX[i] = -600
-                brickY[i] = -6000
-                ballY_change *= -1
-                scoreValue += 1
+    # # collision detection with masks
+    # for i in range (brickMasks):
+    #     result = ballMask.overlap(brickMasks[i])
+    #     if result:
+    #         brickMasks.pop(brickMasks[i]) 
+
+
+
+
+    
+
+    # #Ball and Brick Collision
+    # for i in range (len(brickCoordinates)):
+    #     if ballCircle.colliderect(brickPolygon[i]):
+    #         if abs(ballCircle.top - brickPolygon[i].bottom < 10) and ballY_change < 0:
+    #             brickSound = mixer.Sound("assets/sounds/hitBrickSoundEffect.wav")
+    #             brickSound.play()
+    #             brickCoorX[i] = -600
+    #             brickCoorY[i] = -6000
+    #             ballY_change *= -1
+    #             scoreValue += 1
 
     showScore(textX,textY)
     pygame.display.update()
--- test.py
+++ test.py
@@ -1,108 +1,418 @@
-import os
+# import pygame
+# import random
+
+# # --- classes ---
+
+# class Brick():
+
+#     def __init__(self, x, y, image):
+#         self.image = image
+#         self.rect = self.image.get_rect(x=x, y=y)
+#         self.x = x
+#         self.y = y
+#         self.x_change = 0
+#         self.y_change = 1
+
+#     def draw(self, screen):
+#         self.rect.x = int(self.x)
+#         self.rect.y = int(self.y)
+#         screen.blit(self.image, self.rect)
+#         pygame.draw.rect(screen, (0, 0, 0), self.rect, 1)
+
+#     def update(self):
+#         self.y += self.y_change
+#         self.rect.y = int(self.y)
+
+#         if self.rect.y <= 0:
+#             self.y_change = 1
+#         elif self.rect.y >= 500:
+#             self.y_change = -1
+
+# class Ball():
+
+#     def __init__(self):
+#         #self.image = pygame.image.load("Ball.png")
+
+#         self.image = pygame.Surface((16, 16)).convert_alpha()
+#         self.image.fill((0,0,0,0)) # transparent background
+#         pygame.draw.circle(self.image, (255,255,255), (8, 8), 8)
+
+#         self.rect = self.image.get_rect(centerx=380, centery=280)
+#         self.x = 380
+#         self.y = 280
+#         self.x_change = 3
+#         self.y_change = 3
+
+#     def reset(self):
+#         self.x = 380
+#         self.y = 280
+
+#     def draw(self, screen):
+#         self.rect.centerx = int(self.x)
+#         self.rect.centery = int(self.y)
+#         screen.blit(self.image, self.rect)
+
+#     def update(self):
+#         # Ball Movement and boundary checking
+#         self.x += self.x_change
+#         self.rect.centerx = int(self.x)
+
+#         if self.rect.left <= 0:
+#             self.x_change *= -1
+#         elif self.rect.right >= 800:
+#             self.x_change *= -1
+
+#         self.y += self.y_change
+#         self.rect.centery = int(self.y)
+
+#         if self.rect.top <= 0:
+#             self.y_change *= -1
+#         elif self.rect.bottom >= 600:
+#             self.reset()
+
+# class Paddle():
+
+#     def __init__(self):
+#         #self.image = pygame.image.load("scaledPaddle.png")
+#         self.image = pygame.Surface((100, 30))
+#         self.image.fill((255,0,0))
+
+#         self.rect = self.image.get_rect(x=335, y=550)
+#         self.x_change = 0
+#         self.y_change = 0
+
+#     def reset(self):
+#         self.rect.x = 335
+#         self.rect.y = 550
+
+#     def draw(self, screen):
+#         screen.blit(self.image, self.rect)
+
+#     def update(self):
+#         # Checking boudries of paddle
+#         self.rect.x += self.x_change
+
+#         if self.rect.left <= 0:
+#             self.rect.left = 0
+#         elif self.rect.right >= 800:
+#             self.rect.right = 800
+
+# class Score():
+
+#     def __init__(self):
+#         #self.font = pygame.font.Font("Neufreit-ExtraBold.otf", 24)
+#         self.font = pygame.font.SysFont(None, 24)
+#         self.value = 0
+#         self.x = 10
+#         self.y = 10
+
+#     def reset(self):
+#         self.value = 0
+
+#     def draw(self, screen):
+#         self.image = self.font.render("Score : " + str(self.value), True, (255,255,255))
+#         self.rect = self.image.get_rect(x=self.x, y=self.y)
+#         screen.blit(self.image, self.rect)
+
+# # --- functions ---
+
+# # empty
+
+# # --- main ---
+
+# pygame.init()
+# screen = pygame.display.set_mode((800, 600))
+
+# pygame.display.set_caption("Brick Breaker")
+# #icon = pygame.image.load("Brick Breaker Icon.png")
+# #pygame.display.set_icon(icon)
+
+# # Background Image
+
+# #background_image = pygame.image.load("realBackground.png")
+
+# background_image = pygame.Surface((800,600))
+# for y in range(5, 600, 25):
+#     for x in range(5, 800, 25):
+#         color = random.choice([(255,128,128), (128,255,128), (128,128,255)])
+#         background_image.fill(color, [x,y,15,15])
+
+# # Brick Images
+
+# #brick_images = [
+# #    pygame.image.load("yellowBrick.png"),
+# #    pygame.image.load("greenBrick.png"),
+# #    pygame.image.load("blueBrick.png"),
+# #    pygame.image.load("pinkBrick.png"),
+# #]
+
+# brick_images = [
+#     pygame.Surface((100, 30)),
+#     pygame.Surface((100, 30)),
+#     pygame.Surface((100, 30)),
+#     pygame.Surface((100, 30)),
+#     pygame.Surface((100, 30)),
+#     pygame.Surface((100, 30)),
+# ]    
+
+# brick_images[0].fill((255,0,0))
+# brick_images[1].fill((0,255,0))
+# brick_images[2].fill((0,0,255))
+# brick_images[3].fill((255,255,0))
+# brick_images[4].fill((255,0,255))
+# brick_images[5].fill((0,255,255))
+
+# # Objects
+
+# paddle = Paddle()
+# ball   = Ball()
+# score  = Score()
+
+# # bricks
+# rows_number = 5
+# cols_number = 7
+
+# all_bricks = []
+
+# y = 0
+# for row in range(rows_number):
+#     x = 50
+#     for col in range(cols_number):
+#         color_image = random.choice(brick_images)
+#         brick = Brick(x, y, color_image)
+#         all_bricks.append(brick)
+#         x += 100
+#     y += 30
+
+# # Game Loop
+
+# clock = pygame.time.Clock()
+# running = True
+
+# while running:
+
+#     for event in pygame.event.get():
+#         if event.type == pygame.QUIT:
+#             running = False
+
+#         # If keystroke is pressed check whether left or right
+#         if event.type == pygame.KEYDOWN:
+#             if event.key == pygame.K_LEFT:
+#                 paddle.x_change = -5
+#             if event.key == pygame.K_RIGHT:
+#                 paddle.x_change = 5
+#         if event.type == pygame.KEYUP:
+#             if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
+#                 paddle.x_change = 0
+
+#     # --- updates ---
+
+#     paddle.update()
+#     ball.update()
+
+#     # Bricks Update
+#     for brick in all_bricks:
+#         brick.update()
+
+#     # Ball and Paddle Collision
+#     if ball.rect.colliderect(paddle):
+#         ball.y_change *= -1
+
+#     # Ball and Bricks Collision
+#     for brick in all_bricks:
+#         if ball.rect.colliderect(brick):
+#             brick.x = -400
+#             ball.y_change *= -1
+#             score.value += 1
+
+#     # --- draws ---
+
+#     # To change background colour
+#     # screen.fill((128, 128, 128)) # you don't need it if background fill all screen
+
+#     # background image
+#     screen.blit(background_image, (0, 0))
+
+#     for brick in all_bricks:
+#         brick.draw(screen)
+
+#     paddle.draw(screen)
+#     ball.draw(screen)
+
+#     score.draw(screen)
+
+#     pygame.display.flip()
+
+#     clock.tick(60) # 60 FPS (Frames Per Second) on all computers
+
+# # --- end ---
+
+# pygame.quit() 
+
 import pygame
-from pygame.locals import *
+import sys
+import shapelogic
 
-if not hasattr(pygame, "Mask"):
-    raise "Need pygame 1.8 for masks."
+pygame.init()
 
-pygame.display.init()
-pygame.font.init()
+screensize = width, height = 800, 595
 
-pygame.display.set_caption("Balloon!  Don't hit the walls")
-
-# is the game still going?
-going = 1
-
-screen = pygame.display.set_mode((320,200))
-pygame.key.set_repeat(500, 2)
-clock = pygame.time.Clock()
-
-
-# fill the screen with red... for dramatic effect.
-screen.fill((255,0,0))
-pygame.display.flip()
-
-def load_image(i):
-    'load an image from the data directory with per pixel alpha transparency.'
-    return pygame.image.load(os.path.join("data", i)).convert_alpha()
-
-terrain1 = load_image("terrain1.png")
-balloon = load_image("balloon.png")
-
-# create a mask for each of them.
-terrain1_mask = pygame.mask.from_surface(terrain1, 50)
-balloon_mask = pygame.mask.from_surface(balloon, 50)
-
-# this is where the balloon, and terrain are.
-terrain1_rect = terrain1.get_rect()
-balloon_rect = balloon.get_rect()
-
-
-# a message for if the balloon hits the terrain.
-afont = pygame.font.Font(None, 16)
-hitsurf = afont.render("Hit!!!  Oh noes!!", 1, (255,255,255))
-
-
-last_bx, last_by = 0,0
+screen = pygame.display.set_mode(screensize)
 
 
 
-# start the main loop.
+background_image =pygame.image.load("/Users/marceason/PycharmProjects/Tetris/Wooden_background.jpg").convert_alpha()
 
-while going:
-    events = pygame.event.get()
-    for e in events:
-        if e.type == QUIT or e.type == KEYDOWN and e.key == K_ESCAPE:
-            going = 0
-        if e.type == pygame.KEYDOWN:
-            # move the balloon around, depending on the keys.
-            if e.key in [K_LEFT]:
-                balloon_rect.x -= 1
-            if e.key in [K_RIGHT]:
-                balloon_rect.x += 1
-                
-            if e.key in [K_UP]:
-                balloon_rect.y -= 1
-            if e.key in [K_DOWN]:
-                balloon_rect.y += 1
+myshape = 0
+stop_movement = 0
+blit_count = 0
+stored_shapes = pygame.sprite.Group()
+stored_shapes_with_coords = []
+extra_blit_required = False
 
 
+index = 0
+count = 0
+listofshapes = []
 
-    # see how far the balloon rect is offset from the terrain rect.
-    bx, by = (balloon_rect[0], balloon_rect[1])
-    offset_x = bx - terrain1_rect[0]
-    offset_y = by - terrain1_rect[1]
-    
-    #print bx, by
-    overlap = terrain1_mask.overlap(balloon_mask, (offset_x, offset_y))
+class shapemanager():
 
-    #
-    last_bx, last_by = bx, by
+    def __init__(self):
+
+        self.listofshapes = []
 
 
-    # draw the background color, and the terrain.
-    screen.fill((255,0,0))
-    screen.blit(terrain1, (0,0))
+    def create_another_instance(self):
 
-    # draw the balloon.
-    screen.blit(balloon, (balloon_rect[0], balloon_rect[1]) )
+        global count
+        count += 1
+        string = "Shape_{0},".format(count)
 
-    # draw the balloon rect, so you can see where the bounding rect would be.
-    pygame.draw.rect(screen, (0,255,0), balloon_rect, 1)
+        another_shape = Shape(string)
+        self.listofshapes.append(another_shape)
+        global index
+        object = self.listofshapes[index]
+        index += 1
+        return object
 
 
-    # see if there was an overlap of pixels between the balloon
-    #   and the terrain.    
-    if overlap:
-        # we have hit the wall!!!  oh noes!
-        screen.blit(hitsurf, (0,0))
-
-    # flip the display.
-    pygame.display.flip()
-    
-    # limit the frame rate to 40fps.
-    clock.tick(40)
+def load_shape(self):
+    shape = self.create_another_instance()
+    shape.load_shapes()
 
 
 
-pygame.quit()
+
+
+
+
+class Shape(pygame.sprite.Sprite):
+
+    def __init__(self, name):
+        pygame.sprite.Sprite.__init__(self)
+
+        self.name = name
+        self.x = 50
+        self.y = 100
+        self.move_event = pygame.USEREVENT + 1
+        self.reached_bottom_event = pygame.USEREVENT + 2
+        self.one_sec_timer = 1000
+        self.half_sec_timer = 500
+
+        self.reachbottomflag = False
+        self.movement_possible = True
+
+        self.image = pygame.image.load(
+    "/Users/marceason/PycharmProjects/Tetris/Tetris_Shapes/Green_Shape_1_Position_1.png")
+        self.mask = pygame.mask.from_surface(self.image)
+        self.rect = self.image.get_rect()
+
+
+    def move_shape(self):
+        if self.movement_possible:
+            key_input = pygame.key.get_pressed()
+            if key_input[pygame.K_LEFT]:
+                self.x -= 16
+            if key_input[pygame.K_RIGHT]:
+                self.x += 16
+            if not self.reachbottomflag:
+                if key_input[pygame.K_DOWN]:
+                    self.y += 16
+
+
+    def reachbottom(self):
+
+        if self.y >= 560:
+            self.reachbottomflag = True
+
+    def no_movement_possible(self):
+
+        self.movement_possible = False
+
+def assign_shape():
+
+global myshape
+global stop_movement
+myshape = sl.create_another_instance()
+pygame.time.set_timer(myshape.move_event, myshape.one_sec_timer)
+stop_movement = pygame.time.set_timer(myshape.reached_bottom_event, myshape.half_sec_timer)
+
+def blit_used_shapes():
+
+global screen
+global blit_count
+blit_count = len(stored_shapes_with_coords)
+local_count = 0
+while local_count < blit_count:
+    screen.blit(stored_shapes_with_coords[local_count][0], (stored_shapes_with_coords[local_count][1], stored_shapes_with_coords[local_count][2]))
+    local_count += 1
+
+
+sl = shapemanager()
+
+
+
+
+
+    ##### HERE IS THE PIXEL DETECTION #####
+    result = pygame.sprite.spritecollide(myshape, stored_shapes, False, pygame.sprite.collide_mask)
+
+
+## Main loop ##
+
+assign_shape()
+
+while True:
+
+for event in pygame.event.get():
+    if event.type == pygame.QUIT: sys.exit()
+    screen.blit(background_image, (0, 0))
+    screen.blit(myshape.image, (myshape.x, myshape.y))
+    myshape.move_shape()
+
+
+
+    key_input = pygame.key.get_pressed()
+    if key_input[pygame.K_SPACE]:
+        myshape.rotate_shape()
+
+
+    myshape.reachbottom()
+    if myshape.reachbottomflag:
+        if event.type == myshape.reached_bottom_event:
+            myshape.no_movement_possible()
+            stored_shape_tuple = [myshape.image, myshape.x, myshape.y]
+            stored_shapes_with_coords.append(stored_shape_tuple)
+            stored_shapes.add(myshape)
+
+            extra_blit_required = True
+
+            assign_shape()
+            ####### PIXEL DETECTION IS HERE IN FOR LOOP ####
+            if result:
+                print("this should only execute when two shapes touch!!")
+
+    if extra_blit_required:
+        blit_used_shapes()
+
+    pygame.display.update()
